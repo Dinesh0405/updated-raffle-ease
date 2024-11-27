7ee84b6d5c7078e718e47e352e7dc70bbb05e46e
DJ <dineshjadhav004@gmail.com>
2024-11-27 23:18:15 +0530

    client landing mock changes

--- client/src/app/modules/client/components/pages/landing/landing.component.ts
+++ client/src/app/modules/client/components/pages/landing/landing.component.ts
@@ -18,22 +18,8 @@ import { FooterComponent } from "../../../../admin/components/shared/footer/foot
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-  raffle: Raffle = 
-    {
-        id: 1,
-        title: "Luxury Car Giveaway",
-        description: "Win a brand new luxury car! Don't miss out on this amazing opportunity.",
-        startDate: new Date("2024-11-01T00:00:00Z"),
-        endDate: "2024-12-01T00:00:00Z",
-        status: 'ACTIVE', // assuming RaffleStatus is an enum like: 'active', 'completed', etc.
-        imageKeys: ['http://localhost:4202/assets/images/sample.png'],
-        ticketPrice: 50,
-        availableTickets: 5000,
-        soldTickets: 1500,
-        totalTickets: 5000,
-        revenue: 75000, // 1500 tickets sold * 50 ticket price
-        associationId: 101
-    };
+  raffle!: Raffle
+   
 
   constructor(
     private route: ActivatedRoute,
